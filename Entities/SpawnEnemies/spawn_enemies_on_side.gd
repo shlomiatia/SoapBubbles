be97affcc442d0e842cdb60662f8b657702a2c31
shlomiatia <36788680+shlomiatia@users.noreply.github.com>
2025-02-14 22:14:01 +0200

    cp

--- Entities/SpawnEnemies/spawn_enemies_on_side.gd
+++ Entities/SpawnEnemies/spawn_enemies_on_side.gd
@@ -4,55 +4,54 @@ static var small_enemy_scene: PackedScene = preload("res://Entities/SmallEnemy/S
 static var big_enemy_scene: PackedScene = preload("res://Entities/BigEnemy/BigEnemy.tscn")
 
 static var enemy_props: Dictionary = {
-    SpawnEnemyWave.EnemySize.SMALL: {
-        "scene": small_enemy_scene,
-        "size": 20
-    },
-    SpawnEnemyWave.EnemySize.MEDIUM: {
-        "scene": big_enemy_scene,
-        "size": 32
-    },
-    SpawnEnemyWave.EnemySize.LARGE: {
-        "scene": big_enemy_scene,
-        "size": 64
-    },
-    SpawnEnemyWave.EnemySize.XLARGE: {
-        "scene": big_enemy_scene,
-        "size": 128
-    },
+	SpawnEnemyWave.EnemySize.SMALL: {
+		"scene": small_enemy_scene,
+		"size": 20
+	},
+	SpawnEnemyWave.EnemySize.MEDIUM: {
+		"scene": big_enemy_scene,
+		"size": 32
+	},
+	SpawnEnemyWave.EnemySize.LARGE: {
+		"scene": big_enemy_scene,
+		"size": 64
+	},
+	SpawnEnemyWave.EnemySize.XLARGE: {
+		"scene": big_enemy_scene,
+		"size": 128
+	},
 }
-    
+	
 static func spawn(node: Node, enemies: Array, size_props: Dictionary, formation: SpawnEnemyWave.Formation):
-    
-    var back_multiplier = 1.0
-    var side_multiplier = 0.0
-    if formation == SpawnEnemyWave.Formation.ROW:
-        back_multiplier = 0.0
-        side_multiplier = 1.0
-    var center: Vector2 = size_props["center"]
-    var back_vector: Vector2 = size_props["back_vector"]
-    var side_vector: Vector2 = back_vector.orthogonal().abs()
-    var total_enemies = 0
-    for enemy_group in enemies:
-        total_enemies += enemy_group.count
-    var side_space: float = side_multiplier * size_props["side_size"] / total_enemies
-    prints(formation, enemies)
-    var i = 0
-    for enemy_group in enemies:
-        var enemy_prop: Dictionary = enemy_props[enemy_group.size]
-        var enemy_scene: PackedScene = enemy_prop["scene"]
-        var back_size: float = enemy_prop["size"]
-        var back_space: float = back_size * back_multiplier
-        for j in enemy_group.count:
-            var instance = enemy_scene.instantiate()
-            instance.setup(back_size)
-            node.get_parent().add_child(instance)
-            var side_sign = 1
-            if i % 2 == 0:
-                side_sign = -1
-            var random_direction = Vector2.RIGHT.rotated(randf() * TAU)
-            instance.global_position = center + random_direction * 5.0
-            center += back_vector * back_space  + \
-                      side_sign * side_vector * side_space
-            i = i + 1
-            
+	var center: Vector2 = size_props["center"]
+	var back_vector: Vector2 = size_props["back_vector"]
+	var side_vector: Vector2 = back_vector.orthogonal().abs()
+	var total_enemies = 0
+	for enemy_group in enemies:
+		total_enemies += enemy_group.count
+	var side_space: float = size_props["side_size"] / total_enemies
+	var i = 0
+	var previous_back_space = 0.0
+	var total_back_space = 0.0
+	for enemy_group in enemies:
+		var enemy_prop: Dictionary = enemy_props[enemy_group.size]
+		var enemy_scene: PackedScene = enemy_prop["scene"]
+		var back_size: float = enemy_prop["size"]
+		for j in enemy_group.count:
+			total_back_space += back_size / 2 + previous_back_space / 2
+			previous_back_space = back_size
+			var instance = enemy_scene.instantiate()
+			instance.setup(back_size)
+			node.get_parent().add_child(instance)
+			if formation == SpawnEnemyWave.Formation.COLUMN:
+				instance.global_position = center + back_vector * total_back_space
+			else:
+				var side_sign = 1
+				if i % 2 == 0:
+					side_sign = -1
+				@warning_ignore("integer_division")
+				instance.global_position = center + (i / 2) * side_sign * side_vector * side_space
+			var random_direction = Vector2.RIGHT.rotated(randf() * TAU)	
+			instance.global_position += random_direction * 5.0
+			i = i + 1
+			
