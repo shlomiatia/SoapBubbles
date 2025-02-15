5ccced7d6e77642df593d0f6f5d0b48997dd7fdf
shlomiatia <36788680+shlomiatia@users.noreply.github.com>
2025-02-15 15:59:54 +0200

    cp

--- Entities/SpawnEnemies/spawn_enemies.gd
+++ Entities/SpawnEnemies/spawn_enemies.gd
@@ -4,40 +4,40 @@ var camera: Camera2D
 var cost := 0;
 
 func _ready() -> void:
-	camera = $/root/Game/Player/Camera2D
-	spawn()
-	
+    camera = $/root/Game/Player/Camera2D
+    spawn()
+    
 func spawn() -> void:
-	await get_tree().create_timer(5.0).timeout
-	cost += 8
-	var viewport_size = get_viewport_rect().size
-	var camera_pos = camera.global_position
-	
-	var size_props = {
-		SpawnEnemyWave.SpawnSide.LEFT: {
-			"center": Vector2(camera_pos.x - viewport_size.x / 2.0, camera_pos.y),
-			"back_vector": Vector2(-1.0, 0.0),
-			"side_size": viewport_size.y
-		},
-		SpawnEnemyWave.SpawnSide.RIGHT: {
-			"center": Vector2(camera_pos.x + viewport_size.x / 2.0, camera_pos.y),
-			"back_vector": Vector2(1.0, 0.0),
-			"side_size": viewport_size.y
-		},
-		SpawnEnemyWave.SpawnSide.TOP: {
-			"center": Vector2(camera_pos.x, camera_pos.y - viewport_size.y / 2.0),
-			"back_vector": Vector2(0.0, -1.0),
-			"side_size": viewport_size.x
-		},
-		SpawnEnemyWave.SpawnSide.DOWN: {
-			"center": Vector2(camera_pos.x, camera_pos.y + viewport_size.y / 2.0),
-			"back_vector": Vector2(0.0, 1.0),
-			"side_size": viewport_size.x
-		}
-	}
+    await get_tree().create_timer(5.0).timeout
+    cost += 8
+    var viewport_size = get_viewport_rect().size
+    var camera_pos = camera.global_position
+    
+    var size_props = {
+        SpawnEnemyWave.SpawnSide.LEFT: {
+            "center": Vector2(camera_pos.x - viewport_size.x / 2.0, camera_pos.y),
+            "back_vector": Vector2(-1.0, 0.0),
+            "side_size": viewport_size.y
+        },
+        SpawnEnemyWave.SpawnSide.RIGHT: {
+            "center": Vector2(camera_pos.x + viewport_size.x / 2.0, camera_pos.y),
+            "back_vector": Vector2(1.0, 0.0),
+            "side_size": viewport_size.y
+        },
+        SpawnEnemyWave.SpawnSide.TOP: {
+            "center": Vector2(camera_pos.x, camera_pos.y - viewport_size.y / 2.0),
+            "back_vector": Vector2(0.0, -1.0),
+            "side_size": viewport_size.x
+        },
+        SpawnEnemyWave.SpawnSide.DOWN: {
+            "center": Vector2(camera_pos.x, camera_pos.y + viewport_size.y / 2.0),
+            "back_vector": Vector2(0.0, 1.0),
+            "side_size": viewport_size.x
+        }
+    }
 
-	var configurations = SpawnEnemyWave.generate_spawn_configuration(cost)
-	
-	for config in configurations:
-		SpawnEnemiesOnSide.spawn(self, config.enemies, size_props[config.side], config.formation)
-			
+    var configurations = SpawnEnemyWave.generate_spawn_configuration(cost)
+    
+    for config in configurations:
+        SpawnEnemiesOnSide.spawn(self, config.enemies, size_props[config.side], config.formation)
+            
--- Entities/SpawnEnemies/spawn_enemy_wave.gd
+++ Entities/SpawnEnemies/spawn_enemy_wave.gd
@@ -6,87 +6,87 @@ enum EnemySize { SMALL, MEDIUM, LARGE, XLARGE }
 
 const MIN_ENEMIES_COST = 8
 const SIZE_COSTS = {
-	EnemySize.SMALL: 1,
-	EnemySize.MEDIUM: 4,
-	EnemySize.LARGE: 8,
-	EnemySize.XLARGE: 16
+    EnemySize.SMALL: 1,
+    EnemySize.MEDIUM: 4,
+    EnemySize.LARGE: 8,
+    EnemySize.XLARGE: 16
 }
 
 static func generate_spawn_configuration(total_value: int) -> Array:    
-	var configurations = []
-	
-	var selected_sides = generate_sides(total_value)
-	
-	for side in selected_sides:
-		if side == SpawnSide.TOP or side == SpawnSide.DOWN:
-			total_value /= 2
-			
-	@warning_ignore("integer_division")
-	var points_per_side = total_value / selected_sides.size()
-	
-	for side in selected_sides:
-		var points_for_side = points_per_side
-		var formation = Formation.COLUMN
-		if points_for_side >= MIN_ENEMIES_COST * 2:
-			formation = Formation.ROW if randi() % 2 == 0 else Formation.COLUMN
-			if formation == Formation.ROW:
-				points_for_side /= 2
-	
-		var enemies = generate_enemies_for_side(points_for_side)
-		
-		if enemies.size() > 0:
-			configurations.append({
-				"side": side,
-				"formation": formation,
-				"enemies": enemies
-			})
-	
-	return configurations
+    var configurations = []
+    
+    var selected_sides = generate_sides(total_value)
+    
+    for side in selected_sides:
+        if side == SpawnSide.TOP or side == SpawnSide.DOWN:
+            total_value /= 2
+            
+    @warning_ignore("integer_division")
+    var points_per_side = total_value / selected_sides.size()
+    
+    for side in selected_sides:
+        var points_for_side = points_per_side
+        var formation = Formation.COLUMN
+        if points_for_side >= MIN_ENEMIES_COST * 2:
+            formation = Formation.ROW if randi() % 2 == 0 else Formation.COLUMN
+            if formation == Formation.ROW:
+                points_for_side /= 2
+    
+        var enemies = generate_enemies_for_side(points_for_side)
+        
+        if enemies.size() > 0:
+            configurations.append({
+                "side": side,
+                "formation": formation,
+                "enemies": enemies
+            })
+    
+    return configurations
 
 static func generate_sides(value: int) -> Array:
-	var sides = [SpawnSide.LEFT, SpawnSide.RIGHT, SpawnSide.TOP, SpawnSide.DOWN]
-	var divider = 4
-	
-	@warning_ignore("integer_division")
-	while !sides.is_empty() && value / sides.size() / divider < MIN_ENEMIES_COST:
-		var i = randi() % sides.size()
-		if sides[i] == SpawnSide.TOP || sides[i] == SpawnSide.DOWN:
-			divider /= 2
-		sides.remove_at(i)
-	if sides.size() == 0:
-		sides = [randi() % 2]
-	
-	var num_sides = randi() % sides.size() + 1
-	sides.shuffle()
-	return sides.slice(0, num_sides)
+    var sides = [SpawnSide.LEFT, SpawnSide.RIGHT, SpawnSide.TOP, SpawnSide.DOWN]
+    var divider = 4
+    
+    @warning_ignore("integer_division")
+    while !sides.is_empty() && value / sides.size() / divider < MIN_ENEMIES_COST:
+        var i = randi() % sides.size()
+        if sides[i] == SpawnSide.TOP || sides[i] == SpawnSide.DOWN:
+            divider /= 2
+        sides.remove_at(i)
+    if sides.size() == 0:
+        sides = [randi() % 2]
+    
+    var num_sides = randi() % sides.size() + 1
+    sides.shuffle()
+    return sides.slice(0, num_sides)
 
 
 static func generate_enemies_for_side(points: int) -> Array:
-	var enemies = []
-	var remaining_points = points
-	
-	while remaining_points >= SIZE_COSTS[EnemySize.SMALL]:
-		var available_sizes = []
-		for size in EnemySize.values():
-			if SIZE_COSTS[size] <= remaining_points:
-				available_sizes.append(size)
-		
-		if available_sizes.is_empty():
-			break
-			
-		var chosen_size = available_sizes[randi() % available_sizes.size()]
-		var cost = SIZE_COSTS[chosen_size]
-		
-		var max_count = remaining_points / cost
-		var count = randi() % max_count + 1
-		
-		if !enemies.is_empty() && enemies[enemies.size() - 1].size == chosen_size:
-			enemies[enemies.size() - 1].count += count
-		else:
-			enemies.append({
-				"size": chosen_size,
-				"count": count
-			})
-		
-		remaining_points -= count * cost
-	return enemies
+    var enemies = []
+    var remaining_points = points
+    
+    while remaining_points >= SIZE_COSTS[EnemySize.SMALL]:
+        var available_sizes = []
+        for size in EnemySize.values():
+            if SIZE_COSTS[size] <= remaining_points:
+                available_sizes.append(size)
+        
+        if available_sizes.is_empty():
+            break
+            
+        var chosen_size = available_sizes[randi() % available_sizes.size()]
+        var cost = SIZE_COSTS[chosen_size]
+        
+        var max_count = remaining_points / cost
+        var count = randi() % max_count + 1
+        
+        if !enemies.is_empty() && enemies[enemies.size() - 1].size == chosen_size:
+            enemies[enemies.size() - 1].count += count
+        else:
+            enemies.append({
+                "size": chosen_size,
+                "count": count
+            })
+        
+        remaining_points -= count * cost
+    return enemies
